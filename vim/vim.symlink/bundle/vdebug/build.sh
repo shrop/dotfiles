9d572b90034102798079971967628764d9b13afc
Mark Shropshire <mdshrops@shropnet.net>
2016-04-03 08:42:12 -0400

    Removed autoload and plugged folders

--- vim/vim.symlink/bundle/vdebug/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-version=`cat VERSION`
-echo "Building vdebug version $version"
-tar -cvzf vdebug-$version.tar.gz doc/Vdebug.txt plugin syntax tests CHANGELOG LICENCE README.md requirements.txt vdebugtests.py VERSION
